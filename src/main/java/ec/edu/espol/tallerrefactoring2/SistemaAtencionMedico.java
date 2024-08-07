9a513b06ef5df4a005dce6e4c85c69a39238fbf1
MatiasCollaguazo <mcollagu@espol.edu.ec>
2024-08-07 16:27:51 -0500

    Encapsulate Collecion y Replace Magic Number

--- src/main/java/ec/edu/espol/tallerrefactoring2/SistemaAtencionMedico.java
+++ src/main/java/ec/edu/espol/tallerrefactoring2/SistemaAtencionMedico.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SistemaAtencionMedico {
+    private static final double DESCUENTO_ADULTOS_MAYORES = 0.25;
     private List<Paciente> pacientes;
     private List<Medico> medicos;
     private List<ServicioMedico> serviciosMedicos;
@@ -36,7 +37,7 @@ public class SistemaAtencionMedico {
     public double calcularValorFinalConsulta(double costoConsulta, int edadPaciente){
         double valorARestar = 0;
         if(edadPaciente>=65){
-            valorARestar = costoConsulta*0.25; //0.25 es el descuento para adultos mayores
+            valorARestar = costoConsulta*DESCUENTO_ADULTOS_MAYORES; //0.25 es el descuento para adultos mayores
         }
         return costoConsulta-valorARestar;
     }
